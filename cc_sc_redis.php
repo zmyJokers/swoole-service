ab06fe7d7c4fd1e516f6ce0b8d2995ddbb1097fe
zmy <346277221@qq.com>
2018-07-03 17:40:45 +0800

    Update cc_sc_redis.php

--- cc_sc_redis.php
+++ cc_sc_redis.php
@@ -25,13 +25,13 @@ $client->on("receive", function($cli, $data){
 });
 $client->on("error", function($cli){
 	echo "Connect failed\n";
-	$cli->connect("120.55.113.216",29996,2);
+	$cli->connect("0.0.0.0",1212,2);
 });
 $client->on("close", function($cli){
 	echo "Connection close\n";
-	$cli->connect("120.55.113.216",29996,2);
+	$cli->connect("0.0.0.0",1212,2);
 });
-$client->connect("120.55.113.216",29996,2);
+$client->connect("0.0.0.0",1212,2);
 
 function update_cc($data){ // 更新对应合约
 $http = new swoole_http_client('127.0.0.1', 1215);
